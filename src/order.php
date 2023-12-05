4bc2e6033fe5048a93ca0c4a77c2151d6489390e
imanejarrari <benjarrariimane@gmail.com>
2023-12-06 00:59:14 +0100

    commit

--- src/order.php
+++ src/order.php
@@ -37,6 +37,24 @@ $completedOrdersPercentage = executeSingleValueQuery("SELECT (COUNT(CASE WHEN st
 
 $cancelledOrdersCount = executeSingleValueQuery("SELECT COUNT(*) FROM orders WHERE status='Cancelled'");
 $cancelledOrdersPercentage = executeSingleValueQuery("SELECT (COUNT(CASE WHEN status = 'Cancelled' THEN 1 END) / COUNT(*)) * 100 AS percentage FROM orders");
+
+function getAllCommandes() {
+    global $conn;
+
+    $sql = "SELECT * FROM orders";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $cmd = $result->fetch_all(MYSQLI_ASSOC);
+        return $cmd;
+    } else {
+        return 0;
+    }
+}
+
+$cmd = getAllCommandes();
+$todaysDate = date("Y-m-d");
+
 ?>
 
 
@@ -49,7 +67,42 @@ $cancelledOrdersPercentage = executeSingleValueQuery("SELECT (COUNT(CASE WHEN st
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All orders</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="affichage.css">
+    <style>
+    section {
+            display: flex;
+           margin-left:220px;
+           margin-right:20px;
+           margin-top:30px;
+            align-items: center;
+            flex-wrap: wrap;
+            gap:10px;
+            padding: 10px;
+            justify-content: space-between;
+        }
+        .card {
+           
+            background-color: #f5f5f5;
+            padding: 20px;
+            border-radius: 5px;
+            width: 200px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        }
+        h3 {
+            margin-bottom: 10px;
+        }
+        p {
+            margin: 0;
+        }
+        .tab td{
+            border: 1px solid #041e42;
+            padding: 10px;
+            text-align: center;
+            color: #041e42;
+        }
+    </style>
+
+
 </head>
 <body>
 <div class="main-container">
@@ -84,73 +137,66 @@ $cancelledOrdersPercentage = executeSingleValueQuery("SELECT (COUNT(CASE WHEN st
         <section>
          <div class="card">
                 <h3>Pending Orders</h3>
-                <p><?php echo $pendingOrdersCount; ?> orders : <?php echo $pendingOrdersPercentage; ?>%</p>
+                <p class=""><?php echo $pendingOrdersCount; ?> orders : <?php echo $pendingOrdersPercentage; ?>%</p>
          </div>
          <div class="card">
                 <h3>Completed Orders</h3>
-                <p><?php echo $completedOrdersCount; ?> orders : <?php echo $completedOrdersPercentage; ?>%</p>
+                <p class="completed-status"><?php echo $completedOrdersCount; ?> orders : <?php echo $completedOrdersPercentage; ?>%</p>
          </div>
          <div class="card">
                 <h3>Delivering Orders</h3>
-                <p><?php echo $deliveringOrdersCount; ?> orders : <?php echo $deliveringOrdersPercentage; ?>%</p>
+                <p class="delivering-status"><?php echo $deliveringOrdersCount; ?> orders : <?php echo $deliveringOrdersPercentage; ?>%</p>
          </div>
          <div class="card">
                 <h3>Cancelled Orders</h3>
-                <p><?php echo $cancelledOrdersCount ; ?> orders : <?php echo $cancelledOrdersPercentage; ?>%</p>
+                <p class="cancelled-status"><?php echo $cancelledOrdersCount ; ?> orders : <?php echo $cancelledOrdersPercentage; ?>%</p>
          </div>
 
 
         </section>
         <br>
-        <table>
+        <table border="1" class="tab">
              <th>Command Number</th>
+             <th>User's Id</th>
              <th>User's name</th>
              <th>Total amont</th>
              <th>Order date</th>
              <th>Date of the delivery</th>
              <th>Status</th>
-       - </table>
-  <?php 
-function getAllCommandes() {
-    // Assuming you have a database connection
-    $servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "ecommerce";
+       -
+       <?php
+                 foreach ($cmd as $command) {
+                    $rowClass = '';
+                    switch ($command['status']) {
+                        case 'Completed':
+                            $rowClass = 'completed-status';
+                            break;
+                        case 'Delivering':
+                            $rowClass = 'delivering-status';
+                            break;
+                        case 'Cancelled':
+                            $rowClass = 'cancelled-status';
+                            break;
+                        default:
+                            $rowClass = 'incomplete-status';
+                            break;
+                    }
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Assuming you have a table named 'commands' in your database
-    $sql = "SELECT * FROM commands";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // Fetching the data into an associative array
-        $commands = $result->fetch_all(MYSQLI_ASSOC);
-        return $commands;
-    } else {
-        return 0; // If no commands are found
-    }
-}
- $cmd=getAllCommandes();
- if ($Commands!=0){
-    $todaysDate = date("Y-m-d");
-     foreach ($Commands as $command) {
-
-    }}
-
- ?> 
-   <tr  <?php if(($command['dilivry_date']<$todaysDate ) AND ($command['status']!="Completed")){
-		echo "class=red_row";
-	}?>>
-
-   </tr>
- 
-    
+                    echo '<tr ' . $rowClass . '>';
+                    echo '<td>' . $command['id'] . '</td>';
+                    echo '<td>' . $command['user_id'] . '</td>';
+                    echo '<td>' . $command['username'] . '</td>';
+                    echo '<td>' . $command['total_amount'] . '</td>';
+                    echo '<td>' . $command['order_date'] . '</td>';
+                    echo '<td>' . $command['delivery_date'] . '</td>';
+                    echo '<td>' . $command['status'] . '</td>';
+                    echo '</tr>';
+                }
+                ?>
 
         
 
-         
+        </table>  
 </body>
 <script  src="admin.js"></script>
 </html>
